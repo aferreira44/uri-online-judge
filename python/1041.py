219c17d3bc69c098a4f42a2e9bcca90e7a04f17f
André Ferreira <contato@andreferreira.me>
2018-04-18 23:03:33 -0300

    Refactor challenge 1041

--- python/1041.py
+++ python/1041.py
@@ -4,9 +4,9 @@ y = float(nums[1])
 
 if (x == 0 and y == 0):
   print('Origem')
-elif (x == 0 and y != 0):
+elif (x == 0):
   print('Eixo Y')
-elif (y == 0 and x != 0):
+elif (y == 0):
   print('Eixo X')
 elif (x > 0 and y > 0):
   print('Q1')
